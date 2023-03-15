282ed4644a695fe3480847789d2e8ca7d7b2e9da
kira-ng <tuanna1906@gmail.com>
2023-03-15 21:33:39 +0700

    Building the Side Bar Component (3/5)

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+'use client'
+
+import { signOut, useSession } from 'next-auth/react'
 import NewChat from './NewChat'
 
 function SideBar() {
+  const { data: session } = useSession()
+
   return (
     <div className="flex flex-col p-2 h-screen">
       <div className="flex-1">
@@ -11,6 +15,15 @@ function SideBar() {
 
         {/* Map through the ChatRows */}
       </div>
+
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image!}
+          alt="Profile pic"
+          className="h-12 w-12 rounded-full mx-auto cursor-pointer mb-2 hover:opacity-50"
+        />
+      )}
     </div>
   )
 }
