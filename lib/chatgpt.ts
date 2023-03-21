36837a13b2be06834fb1500ab261a9ecea1c03f4
kira-ng <tuanna1906@gmail.com>
2023-03-21 12:19:02 +0700

    Setting up OpenAI API Key (2/3)

---
+++ lib/chatgpt.ts
@@ -0,0 +1,8 @@
+import { Configuration, OpenAIApi } from 'openai'
+
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+const openAI = new OpenAIApi(config)
+
+export default openAI
