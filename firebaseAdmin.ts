e677c24f9a7fb4dec2761fb2062b30ab95c090c4
kira-ng <tuanna1906@gmail.com>
2023-03-21 13:12:05 +0700

    Setting up OpenAI API Key (3/3)

--- firebaseAdmin.ts
+++ firebaseAdmin.ts
@@ -1,12 +1,15 @@
 import admin from 'firebase-admin'
+import { getApps } from 'firebase-admin/app'
 
 var serviceAccount = JSON.parse(
   process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
 )
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-})
+if (!getApps().length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  })
+}
 
 const adminDb = admin.firestore()
 
